6b6e0f5940a254e9bdd9079cad520d856db40be0
Braeden Audlin <25baudlin@msjnet.net>
2025-01-29 21:20:52 -0500

    inital commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+  static void towers_of_hanoi(int n, int source, int target, int aux) {
+    if (n == 1) {
+      System.out.printf("Move disk from {} to {}", source, target);
+      return;
+    }
+  }
+}
